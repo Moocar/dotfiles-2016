705b58cf632d6e7776b6f14271a03c7cf70d83c9
Anthony Marcar <amarcar@walmartlabs.com>
2015-08-01 10:05:37 -0700

    remove melpa

--- install_elpa.el
+++ install_elpa.el
@@ -5,6 +5,11 @@
 (add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (package-refresh-contents)
@@ -26,6 +31,7 @@
     yasnippet
     org
     markdown-mode
+    paredit
     ))
 
 (dolist (p my-packages)
